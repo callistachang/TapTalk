2fe4959dbf204cf396209304d971c63aebf2d96a
callistachang <rezinomrah@gmail.com>
2020-03-19 16:19:04 +0800

    Add views

---
+++ taptalk/views.py
@@ -0,0 +1,50 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from articles.models import Article
+from users.models import User, Expert
+from comments.models import Comment
+
+class MainView(generic.ListView):
+    template_name = 'main.html'
+    context_object_name = 'article_list'
+
+    def get_queryset(self):
+        return Article.objects.all()
+
+# TODO: Fetch comments as well
+# TODO: Change to class-based view?
+def article(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    context ={
+        'article': article,
+        'error_msg': None
+    }
+    return render(request, 'article.html', context)
+
+# TODO: LinkedIn and FB integration
+class ProfileView(generic.DetailView):
+    model = User
+    template_name = 'profile.html'
+
+# TODO: User is hardcoded to be Obama, section is hardcoded to 1
+def post_comment(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    content = request.POST['Comment']
+    user = User.objects.get(pk=2)
+
+    if not Comment.objects.create_comment(content, user, article, 1):
+        context = {
+            'article': article,
+            'error_msg': 'You are not allowed to post that comment as it is toxic.'
+        }
+        return render(request, 'article.html', context)
+
+    # Comment.objects.create_comment(
+    #         content=content, article=article, creator=user)
+    # except:
+    #     print("Oops")
+
+    return HttpResponseRedirect(reverse('article', args=(article_id,)))
