f4ddd8fedefaa72b1f78b66fa409a19130ce11fc
SnowJanauary <TOHJ0032@e.ntu.edu.sg>
2020-04-11 01:27:31 +0800

    Add files via upload

---
+++ Content/javascript/test.js
@@ -0,0 +1,32 @@
+﻿$(document).ready
+(
+    function ()
+    {
+        $(".toggleTapTalk").click
+            (
+            function()
+            {
+                toggleOverlay();
+            }
+            );
+    }
+);
+
+
+
+var isOverlay = false;
+
+function toggleOverlay()
+{
+    if (!isOverlay)
+    {
+        isOverlay = true;
+        $(".tutorialOverlay").css("display", "block");
+    }
+
+    else
+    {
+        isOverlay = false;
+        $(".tutorialOverlay").css("display", "none");
+    }
+}
